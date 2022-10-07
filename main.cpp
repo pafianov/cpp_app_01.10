ee8d5a573ee4d8fa7560f904902c40157efd452b
s.pafianov <s.pafianov@SERVICE>
2022-10-07 18:22:24 +0300

    Done task #1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+//--------------------------------------------------------------------#
+// Task 1. The user enters the distance from the keyboard             |
+//		   to the airportand the time it takes to get there.Calculate |
+//		   the speed at which he needs to travel.                     |
+//--------------------------------------------------------------------#
+
 #define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
@@ -5,7 +11,12 @@ using namespace std;
 
 int main()
 {
-
-
+	double x,y;
+	cout << "Distanse (km) -> ";
+	cin >> x;
+	cout << "Time (hour) -> ";
+	cin >> y;
+	double res = x * y;
+	cout << "Speed = " << res << " km/h" << endl;
 	return 0;
 }
