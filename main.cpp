44b5a4131b3eeaa4176e14d3c35f79e2b3b7c6a8
s.pafianov <s.pafianov@SERVICE>
2022-10-08 12:54:01 +0300

    Done task # 4

--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@ using namespace std;
 
 int main()
 {
-
+	const int time = 86400;
+	int a;
+	cout << "Enter the time in seconds elapsed since the beginning of the day" << endl;
+	cin >> a;
+	int hh = a % time / 3600;
+	int mm = a / 60 % 60;
+	int ss = a % 60;
+	int endhh, endmm, endss;
+	int tmp = hh * 3600 + mm * 60 + ss;
+	tmp = time - tmp;
+	endhh = tmp / 3600;
+	endmm = tmp / 60 - endhh * 60;
+	endss = tmp - endmm * 60 - endhh * 3600;
+	cout << "Now is: " << hh << " hh: " << mm << " mm: " << ss << " ss" << endl;
+	cout << "before the midnight: " << endhh << " hh: " << endmm << " mm: " << endss << " ss" << endl;
 
 	return 0;
 }
